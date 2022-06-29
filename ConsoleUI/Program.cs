1d0d6ab7c3b61ef0b47b31ce39df2736f7098931
fs2016g <arcturussoftware.io>
2022-06-29 17:05:19 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TableServers servers = new TableServers();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("The next server is: " + servers.GetNextServer());
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("The next server is: " + servers.GetNextServer());
+        }
+    }
+}
---
+++ ConsoleUI/TableServers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class TableServers
+    {
+        private List<string> servers = new List<string>();
+        private int nextServer = 0;
+
+        public TableServers()
+        {
+            servers.Add("Tim");
+            servers.Add("Sue");
+            servers.Add("Mary");
+            servers.Add("Bob");
+        }
+
+        public string GetNextServer()
+        {
+            string output = servers[nextServer];
+            nextServer++;
+
+            if(nextServer >= servers.Count)
+            {
+                nextServer = 0;
+            }
+            return output;
+        }
+    }
+}
